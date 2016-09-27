0388f7d0d4e416e1b4aa9e8153899757dcc48eaf
ducaoga <gailchan16@gmail.com>
2016-09-27 14:27:23 +0800

    Update!

---
+++ home.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	</head>
+	<body>
+		<button class="btn btn-danger">EXIT!</button>
+		<i class="glyphicon glyphicon-remove"></i>
+	</body>
+</html>
